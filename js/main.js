46213a7e9f42712ee7ee75e97ec8081a862d6736
Vitoria Costa <vitoriacosta.contato@gmail.com>
2020-07-02 20:31:14 -0300

    primeiro loop for

--- js/main.js
+++ js/main.js
@@ -1,30 +1,4 @@
 // comparar valores usa-se == : true == true > true; diferente !=; > e < e >= e <= também são usados
-var PremiumUser = false; 
-
-if(PremiumUser == true){
-    console.log("Thanks for your subscription!");
+for(i = 0; i <= 10; i++){
+    console.log ("Current i is... " , i);
 }
-else{
-    console.log("If you wanna see more from us, subscribe!");
-}
-
-var age = 20;
-
-if(age<1){
-    console.log("you are a baby");
-}
-else if(age<3){
-    console.log("you are a toddler!");
-}
-else if(age <= 10){
-    console.log ("kid");
-}
-else if(age <= 19){
-    console.log("teen");
-}
-else if (age <= 60){
-    console.log("adult");
-}
-else {
-    console.log("elderly")
-} 
