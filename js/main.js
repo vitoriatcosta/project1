9fa79c67f8aaf6c1f390632a8c80c85a44f6a8ff
Vitoria Costa <vitoriacosta.contato@gmail.com>
2020-07-02 20:43:27 -0300

    função criada

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-// comparar valores usa-se == : true == true > true; diferente !=; > e < e >= e <= também são usados
-for(i = 0; i <= 10; i++){
-    console.log ("Current i is... " , i);
+function myVar(){
+    
 }
