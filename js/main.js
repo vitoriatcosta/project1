44374d9a8a9c61ef2c53fc2dfe96437f36bfd5ac
Vitoria Costa <vitoriacosta.contato@gmail.com>
2020-07-02 21:32:39 -0300

    async adicionado
    js carregava antes do html, então o objeto constava como null
    agora o js carrega por último, encontrando o elemento

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 function buttonClicked() {
     console.log ("Button clicked.");
+    btn.removeEventListener("click", buttonClicked);
 }
 var btn = document.getElementById("go-button");
 
 //object.method("event", function)
-btn.addEventListener("click", buttonClicked);
+btn.addEventListener ("click", buttonClicked);
