dc712a2ff74f72a48bd242e3ff84d819295a51cd
Vitoria Costa <vitoriacosta.contato@gmail.com>
2020-07-02 21:47:46 -0300

    innerHTML
    muda o texto html pelo js, muito massa
    aula 50

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 function buttonClicked() {
     console.log ("Button clicked.");
     btn.removeEventListener("click", buttonClicked);
+    document.getElementById ("text").innerHTML = "Don't do it."
 }
 var btn = document.getElementById("go-button");
 
 //object.method("event", function)
 btn.addEventListener ("click", buttonClicked);
+//roda só uma vez
